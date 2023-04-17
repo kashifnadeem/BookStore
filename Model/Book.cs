643eca8e68d02e713c13f0a140d00051dc092d7c
kashifnadeem <knadeemm@outlook.com>
2023-04-17 17:46:18 +0200

    Add project files.

---
+++ Model/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace BookStore.Model
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string BookName { get; set; }
+        public int BookQuantity { get; set; }
+
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using BookStore.Model;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BookStore.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public IEnumerable<Book> GetBooks { get; set; }
+        public void OnGet()
+        {
+            GetBooks = GetCustomers();
+        }
+
+        public List<Book> GetCustomers()
+        {
+            List<Book> books = new List<Book>();
+            string connectionString = "Server=tcp:sqlserverkn.database.windows.net,1433;Initial Catalog=sqldbkn;Persist Security Info=False;User ID=kashif;Password=Geneva12;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = con;
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.Clear();
+                    cmd.CommandText = "SELECT * FROM Books";
+                    con.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        books.Add(new Book()
+                        {
+                            BookId = Convert.ToInt32(rdr["BookId"]),
+                            BookName = rdr["BookName"].ToString(),
+                            BookQuantity = Convert.ToInt32(rdr["BookQuantity"])
+                        });
+                    }
+                    con.Close();
+                }
+            }
+
+            return books;
+        }
+    }
+}
